1a1e6ec14b76f89a70c2dbdd0eec81e5585a0acd
wavememory88 <wave.memory.88@gmail.com>
2022-01-17 01:04:47 +0700

    first commit

---
+++ Assets/MyEnemySpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyEnemySpawner : MonoBehaviour
+{
+
+    public GameObject spawnPoint;
+    public GameObject enemyPrefab;
+    public float secondsBetweenSpawns;
+    float secondsSinceLastSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        secondsSinceLastSpawn = 0;
+        spawnPoint.transform.position = transform.position;
+        spawnPoint.transform.rotation = transform.rotation;
+    }
+
+    //Fixed update happens the same number of times for all players, so it's a good place for gameplay critical things.
+    private void FixedUpdate()
+    {
+        secondsSinceLastSpawn += Time.fixedDeltaTime;
+        if (secondsSinceLastSpawn >= secondsBetweenSpawns)
+        {
+            Instantiate(enemyPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+            secondsSinceLastSpawn = 0;
+        }
+    }
+}
